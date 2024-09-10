3bab12e70c3909c072601d31dee4a2a70d1fc19d
New User <newuser@example.com>
2024-09-10 09:03:35 +0300

    themechange

--- context/themeContext.js
+++ context/themeContext.js
@@ -1,38 +1,38 @@
-"use client";
-import React, { useState, createContext, useEffect } from "react";
+'use client'
+import React, { useState, createContext, useEffect } from 'react'
 
-export const ThemeContext = createContext();
+export const ThemeContext = createContext()
 
 const ThemeProvider = ({ children }) => {
-    const [theme, setTheme] = useState("dark");
+  const [theme, setTheme] = useState('light')
 
-    // Toggle Theme
-    const setThemeFun = () => {
-        if (theme === "dark") {
-            setTheme("light");
-            localStorage.setItem("myPortfolioProfileTheme", "light");
-        } else {
-            setTheme("dark");
-            localStorage.setItem("myPortfolioProfileTheme", "dark");
-        }
-    };
+  // Toggle Theme
+  const setThemeFun = () => {
+    if (theme === 'light') {
+      setTheme('dark')
+      localStorage.setItem('myPortfolioProfileTheme', 'dark')
+    } else {
+      setTheme('light')
+      localStorage.setItem('myPortfolioProfileTheme', 'light')
+    }
+  }
 
-    // Get Theme Value From LocalStorage
-    useEffect(() => {
-        const getTheme = localStorage.getItem("myPortfolioProfileTheme");
-        if (!getTheme) {
-            return
-        }
-        setTheme(getTheme);
-    }, []);
+  // Get Theme Value From LocalStorage
+  useEffect(() => {
+    const getTheme = localStorage.getItem('myPortfolioProfileTheme')
+    if (!getTheme) {
+      return
+    }
+    setTheme(getTheme)
+  }, [])
 
-    return (
-        <ThemeContext.Provider value={{ theme, setThemeFun }}>
-            <div className={theme === "dark" ? "dark" : ""}>
-                <div className='dark:text-white dark:bg-black'>{children}</div>
-            </div>
-        </ThemeContext.Provider>
-    );
-};
+  return (
+    <ThemeContext.Provider value={{ theme, setThemeFun }}>
+      <div className={theme === 'dark' ? 'dark' : ''}>
+        <div className="dark:text-white dark:bg-black">{children}</div>
+      </div>
+    </ThemeContext.Provider>
+  )
+}
 
-export default ThemeProvider;
+export default ThemeProvider
